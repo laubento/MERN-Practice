4ec5c0e97017c55b45d5a9a30bf0df4f6ab44b19
laubento <lauiribarrenn@gmail.com>
2023-04-03 11:12:38 -0300

    Create firts two route

--- index.ts
+++ index.ts
@@ -10,12 +10,14 @@ const port: string | number = process.env.PORT || 8000
 
 // Define the first Route of APP
 app.get('/' , (req: Request, res: Response) => {
-    res.send('APP Express + TS + Swagger + Mongoose')
+    res.send('Goodbye, world')
 })
 
 // Define the second Route of APP
 app.get('/hello' , (req: Request, res: Response) => {
-    res.send('hello word')
+    let user = req.query.user || "anonimo"
+    console.log(user)
+    res.send('hello ' + user)
 })
 
 
