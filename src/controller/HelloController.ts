d6924708c19e5237d1e1f9abc028cdc9fbbba52f
laubento <lauiribarrenn@gmail.com>
2023-04-06 10:59:10 -0300

    Swagger issue & Moongose Schema Ready

--- src/controller/HelloController.ts
+++ src/controller/HelloController.ts
@@ -9,12 +9,12 @@ import { Get, Query, Route, Tags } from "tsoa";
 export class HelloController implements IHelloController {
     /**
      * Endopoint to retreive a Message "Hello Name" in JSON
-     * @param name Name of usuer to be greeted
-     * @returns Promise of BasicResponse
+     * @param {string | undefined} name Name of usuer to be greeted
+     * @returns { BasicResponse }Promise of BasicResponse
      */
 
     @Get("/")
-    public async getMessage(name?: string): Promise<BasicResponse> {
+    public async getMessage(@Query()name?: string): Promise<BasicResponse> {
         LogSuccess('[/api/hello] Get Request')
 
         return {
